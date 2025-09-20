e29a180f4b646c30907d7ae67efbf992cbd29bcd
Jack WebDev <jackbuda31@gmail.com>
2025-09-20 09:02:25 +0200

    :tada: initial commit

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: ".env.development.dist" });
 
 export default defineConfig({
-	schema: "./src/db/schema",
-	out: "./src/db/migrations",
-	dialect: "postgresql",
-	dbCredentials: {
-		url: process.env.DATABASE_URL || "",
-	},
+  schema: "./src/db/schema",
+  out: "./src/db/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL || "",
+  },
+  migrations: {
+    schema: "public",                
+    table: "__drizzle_migrations",    
+  },
 });
