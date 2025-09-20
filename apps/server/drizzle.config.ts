7fb2c274c7ff236535be9cbadd817c023b5c7d73
Jack WebDev <jackbuda31@gmail.com>
2025-09-20 18:55:27 +0200

    update drizzle config

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,10 +1,13 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "drizzle-kit";
-
 import { env } from "../../apps/server/src/utils";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-	schema: "./src/db/schema/**/*.ts",
-	out: "./src/db/migrations",
+	schema: path.join(__dirname, "src/db/schema/**/*.ts"),
+	out: path.join(__dirname, "src/db/migrations"),
 	dialect: "postgresql",
 	dbCredentials: {
 		url: env.DATABASE_URL,
