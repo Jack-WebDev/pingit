4af3ec24e9826e426c0cefced54ad3bccc8fce76
Jack WebDev <jackbuda31@gmail.com>
2025-09-21 15:55:10 +0200

    fix server not found and throwing CORS

--- apps/server/api/[...all].ts
+++ apps/server/api/[...all].ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import type { FastifyInstance } from "fastify";
-import { createServer } from "../src/index";
+import { createServer } from "../src/index.js";
 
 let app: FastifyInstance | null = null;
 
