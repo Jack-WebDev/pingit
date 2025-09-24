169e0e672280281e4415048fd5e94e65df08224c
Jack WebDev <jackbuda31@gmail.com>
2025-09-24 15:42:47 +0200

    update db configs

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -8,16 +8,11 @@ import * as schema from "./schema/index.js";
 const isProd = env.NODE_ENV === "production" || process.env.VERCEL === "1";
 
 let db: ReturnType<typeof drizzleVercel> | ReturnType<typeof drizzleNode>;
-let pool: Pool | undefined;
 
 if (isProd) {
 	db = drizzleVercel(sql, { schema });
 } else {
-	const url =
-		env.POSTGRES_URL_NON_POOLING || env.DATABASE_URL || env.POSTGRES_URL || "";
-
-	pool = url ? new Pool({ connectionString: url, ssl: false }) : new Pool();
-
+	const pool = new Pool();
 	db = drizzleNode(pool, { schema });
 }
 
